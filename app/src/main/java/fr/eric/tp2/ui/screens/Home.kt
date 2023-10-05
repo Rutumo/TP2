b78dfffb12ee28d02d79cd3d1c9bf84bb569a29d
Kennedy <jeclaude@mail.ru>
2023-10-05 04:30:05 +0200

    commit

--- app/src/main/java/fr/eric/tp2/ui/screens/Home.kt
+++ app/src/main/java/fr/eric/tp2/ui/screens/Home.kt
@@ -1,8 +1,6 @@
 package fr.eric.tp2.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
